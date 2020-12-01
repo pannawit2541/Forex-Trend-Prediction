b149d4a0e83632c4385023fa7a9860ac3208df40
pannawit2541 <35355034+pannawit2541@users.noreply.github.com>
2020-12-01 22:56:21 +0700

    add FUZZY

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np 
+import numpy as np
 from datetime import datetime
 
 import talib
@@ -11,47 +11,49 @@ import plotly as py
 from plotly import tools
 import plotly.graph_objects as go
 
-def preprocessing(file,s=0,create_file=False):
+import skfuzzy as fuzz
+
+
+def preprocessing(file, s=0, create_file=False):
 
     #################################################
 
-    #file  : name of data_set
-    #s : start sample at 's' 
+    # file  : name of data_set
+    # s : start sample at 's'
 
-    #return  : Heiken_Ashi OHLC candles
+    # return  : Heiken_Ashi OHLC candles
 
-    ################################################# 
+    #################################################
 
     data = pd.read_csv(file)
     data.set_index('date', inplace=True, drop=True)
-    data = data.iloc[s:,:]
-    data = pd.DataFrame(data=data, dtype=np.float64) 
+    data = data.iloc[s:, :]
+    data = pd.DataFrame(data=data, dtype=np.float64)
 
     df = data.copy(deep=False)
-    df.drop(df.tail(24).index,inplace=True)
-  
-    label = data[['open','close']].copy(deep=False)
-    label = label.iloc[24:,:]
- 
+    df.drop(df.tail(24).index, inplace=True)
 
-    label.reset_index(drop=True,inplace=True)
+    label = data[['open', 'close']].copy(deep=False)
+    label = label.iloc[24:, :]
+
+    label.reset_index(drop=True, inplace=True)
     label.index = df.index
 
-    label.rename(columns={"open": "open_24", "close": "close_24"}, inplace=True)
+    label.rename(columns={"open": "open_24",
+                          "close": "close_24"}, inplace=True)
 
     def Heiken_Ashi(prices):
- 
-        #################################################
-
-        #prices  : dataframe of prices
-        #periods : periods of which to create the candles
-
-        #return  : Heiken_Ashi OHLC candles
 
         #################################################
 
+        # prices  : dataframe of prices
+        # periods : periods of which to create the candles
 
-        HA_close = prices[['open','high','low','close']].sum(axis = 1)/4
+        # return  : Heiken_Ashi OHLC candles
+
+        #################################################
+
+        HA_close = prices[['open', 'high', 'low', 'close']].sum(axis=1)/4
 
         HA_open = HA_close.copy()
 
@@ -61,25 +63,28 @@ def preprocessing(file,s=0,create_file=False):
 
         HA_low = HA_close.copy()
 
-        for i in range(1,len(prices)):
-            
+        for i in range(1, len(prices)):
+
             HA_open.iloc[i] = (HA_open.iloc[i-1] + HA_close.iloc[i-1])/2
 
-            HA_high.iloc[i] = np.array([prices.high.iloc[i], HA_open.iloc[i], HA_close.iloc[i]]).max()
+            HA_high.iloc[i] = np.array(
+                [prices.high.iloc[i], HA_open.iloc[i], HA_close.iloc[i]]).max()
 
-            HA_low.iloc[i] = np.array([prices.low.iloc[i], HA_open.iloc[i], HA_close.iloc[i]]).min()
+            HA_low.iloc[i] = np.array(
+                [prices.low.iloc[i], HA_open.iloc[i], HA_close.iloc[i]]).min()
 
-        return HA_open,HA_high,HA_low,HA_close
+        return HA_open, HA_high, HA_low, HA_close
 
     #------------------------------------------------------------#
     # Momentum (MOM) :
     #------------------------------------------------------------#
 
-    periods = [3,4,5,8,9,10]
+    periods = [3, 4, 5, 8, 9, 10]
     #periods = [x+20 for x in periods]
 
-    for i in range(0,len(periods)):
-        df['MOM_{i}'.format(i=periods[i])] = talib.MOM(df.close.values,timeperiod = periods[i])
+    for i in range(0, len(periods)):
+        df['MOM_{i}'.format(i=periods[i])] = talib.MOM(
+            df.close.values, timeperiod=periods[i])
     print(periods)
     print("--------- Mometum Successful ---------")
 
@@ -87,15 +92,15 @@ def preprocessing(file,s=0,create_file=False):
     # Stochastic oscillator (STOCH):
     #------------------------------------------------------------#
 
-    periods = [3,4,5,8,9,10]
+    periods = [3, 4, 5, 8, 9, 10]
     #periods = [x+20 for x in periods]
-    for i in range(0,len(periods)):
-        K,D = talib.STOCH(
-            close = df['close'],
-            high = df['high'],
-            low = df['low'],
+    for i in range(0, len(periods)):
+        K, D = talib.STOCH(
+            close=df['close'],
+            high=df['high'],
+            low=df['low'],
             fastk_period=12
-            )
+        )
         df['K_{i}'.format(i=periods[i])] = K
         df['D_{i}'.format(i=periods[i])] = D
 
@@ -105,16 +110,16 @@ def preprocessing(file,s=0,create_file=False):
     #------------------------------------------------------------#
     # Williams %R (WILLR) :
     #------------------------------------------------------------#
-    
-    periods = [6,7,8,9, 10]
+
+    periods = [6, 7, 8, 9, 10]
     #periods = [x+20 for x in periods]
     for i in range(len(periods)):
         df['WILLR_{i}'.format(i=periods[i])] = talib.WILLR(
-            high = df['high'],
-            low = df['low'],
-            close = df['close'],
-            timeperiod = periods[i]
-            )
+            high=df['high'],
+            low=df['low'],
+            close=df['close'],
+            timeperiod=periods[i]
+        )
     print(periods)
     print("--------- Williams %R Successful ---------")
 
@@ -122,12 +127,12 @@ def preprocessing(file,s=0,create_file=False):
     #  Rate of change (PROCP) :
     #------------------------------------------------------------#
 
-    periods = [12,13,14,15]
+    periods = [12, 13, 14, 15]
     #periods = [x+20 for x in periods]
     for i in range(len(periods)):
         df['ROCP_{i}'.format(i=periods[i])] = talib.ROCP(
             df['close'],
-            timeperiod = periods[i]
+            timeperiod=periods[i]
         )
 
     print(periods)
@@ -138,10 +143,10 @@ def preprocessing(file,s=0,create_file=False):
     #------------------------------------------------------------#
 
     df['WPC'] = talib.WCLPRICE(
-            high = df['high'],
-            low = df['low'],
-            close = df['close']
-        )
+        high=df['high'],
+        low=df['low'],
+        close=df['close']
+    )
 
     print("--------- Weighted Closing Price Successful ---------")
 
@@ -150,10 +155,10 @@ def preprocessing(file,s=0,create_file=False):
     #------------------------------------------------------------#
 
     df['ADL'] = talib.AD(
-        high = df['high'],
-        low = df['low'],
-        close = df['close'],
-        volume = df['volume']
+        high=df['high'],
+        low=df['low'],
+        close=df['close'],
+        volume=df['volume']
     )
 
     print("--------- Accumulation Distribution Line Successful ---------")
@@ -162,28 +167,28 @@ def preprocessing(file,s=0,create_file=False):
     # Accumulation Distribution Oscillator (ADOSC) :
     #------------------------------------------------------------#
 
-    periods_fast = [2,3,4,5]
+    periods_fast = [2, 3, 4, 5]
     #periods_fast = [x+20 for x in periods_fast]
-    periods_slow = [10,12,14,16]
+    periods_slow = [10, 12, 14, 16]
     #periods_slow = [x+20 for x in periods_slow]
     for i in range(len(periods_fast)):
-        df['ADOSC_{i},{j}'.format(i=periods_fast[i],j=periods_slow[i])] = talib.ADOSC(
-            high = df['high'],
-            low = df['low'],
-            close = df['close'],
-            volume = df['volume'],
-            fastperiod = periods_fast[i],
-            slowperiod = periods_slow[i]
+        df['ADOSC_{i},{j}'.format(i=periods_fast[i], j=periods_slow[i])] = talib.ADOSC(
+            high=df['high'],
+            low=df['low'],
+            close=df['close'],
+            volume=df['volume'],
+            fastperiod=periods_fast[i],
+            slowperiod=periods_slow[i]
         )
-    
+
     print("--------- Accumulation Distribution Line Successful ---------")
 
     #------------------------------------------------------------#
     # Moving Average Convergence/Divergence (MACD) :
     #------------------------------------------------------------#
-    
+
     indicator_MACD = MACD(
-        close = df['close'],
+        close=df['close'],
         n_fast=12,
         n_slow=26,
         n_sign=9,
@@ -195,16 +200,16 @@ def preprocessing(file,s=0,create_file=False):
     df['MACD_signal_12,26'] = indicator_MACD.macd_signal()
 
     print("--------- Moving Average Convergence/Divergence Successful ---------")
-    
+
     #------------------------------------------------------------#
     # Commodity Channel Index (CCI) :
     #------------------------------------------------------------#
 
     df['CCI_15'] = talib.CCI(
-        high = df['high'],
-        low = df['low'],
-        close = df['close'],
-        timeperiod = 15        
+        high=df['high'],
+        low=df['low'],
+        close=df['close'],
+        timeperiod=15
     )
 
     print("--------- Commodity Channel Index Successful ---------")
@@ -224,7 +229,7 @@ def preprocessing(file,s=0,create_file=False):
     # Heikin Ashi :
     #------------------------------------------------------------#
 
-    Open,High,Low,Close = Heiken_Ashi(df)
+    Open, High, Low, Close = Heiken_Ashi(df)
     df['HA_open'] = Open
     df['HA_high'] = High
     df['HA_low'] = Low
@@ -236,14 +241,14 @@ def preprocessing(file,s=0,create_file=False):
     # Relative Strange index (RSI) :
     #------------------------------------------------------------#
 
-    periods = [6,8,10,12]
+    periods = [6, 8, 10, 12]
     #periods = [x+20 for x in periods]
     for i in range(len(periods)):
         df['RSI_{i}'.format(i=periods[i])] = talib.RSI(
             df['close'],
-            timeperiod = periods[i]
+            timeperiod=periods[i]
         )
-    
+
     print(periods)
     print("--------- Relative Strange index Successful ---------")
 
@@ -256,17 +261,80 @@ def preprocessing(file,s=0,create_file=False):
 
     print("--------- Slope Successful ---------")
 
-    
-    df = df.drop(['volume'],axis=1)
+    df = df.drop(['volume'], axis=1)
 
-    #----------- Create File .csv------------
+    # ----------- Create File .csv------------
     if create_file == True:
-        _csv = pd.concat([df,label],axis = 1)
+        _csv = pd.concat([df, label], axis=1)
         _csv.to_csv(r'dataset/EURUSD_features.csv')
 
-    return df,label
+    return df, label
+
+def smf(x, a, b):
+
+    ###################################################
+    # x : input
+    # a : minimum number
+    # b : maximum number
+    ###################################################
+
+    if x < a:
+        return 0
+    elif a <= x and x <= (a+b)/2:
+        return 2*((x-a)/(b-a))**2
+    elif (a+b)/2 <= x and x <= b:
+        return 1-(2*((b-x)/(b-a)))**2
+    else:
+        return 1
 
 
+file = 'dataset/EURUSD_features.csv'
+#X,Y = preprocessing(file,create_file=True)
+data = pd.read_csv(file)
+data.set_index('date', inplace=True, drop=True)
 
-file = 'dataset/EURUSD_H1.csv'
-X,Y = preprocessing(file,create_file=True)
+################################################
+'''
+    - 4 years data
+    - 4 y * 356 d * 24 hr = 34,176
+
+'''
+amounts = 34176
+data = data.tail(amounts+24)
+data = pd.DataFrame(data=data, dtype=np.float64)
+##############################################
+
+df = data.copy(deep=False)
+df.drop(df.tail(24).index, inplace=True)
+df = df.drop(['open_24', 'close_24'], axis=1)
+
+label = data[['open_24', 'close_24']].copy(deep=False)
+label = label.iloc[24:, :]
+
+label.reset_index(drop=True, inplace=True)
+label.index = df.index
+
+from sklearn.model_selection import KFold
+kf = KFold(n_splits=12)
+kf.get_n_splits(df)
+
+i = 0
+for _, b in kf.split(df):
+    print("============================")
+    print(df.iloc[b].head())
+    a = round(smf(i,0,12),3)
+    df.iloc[b] = df.iloc[b]*a 
+    print(df.iloc[b].head())
+    i+=1
+
+
+    
+#     print(len(x))
+    
+# start = 1
+# end = 12
+# for i in range(start,end+1):
+#     a = round(smf(i,0,12),3)
+#     print(a)
+# #print(a)
+
